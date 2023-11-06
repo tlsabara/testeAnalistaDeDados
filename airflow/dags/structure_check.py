232490165a36b5e323f6303770d756ba6a5a8d8f
Thiago Sabara <thi.sil.sab@gmail.com>
2023-11-05 23:38:39 -0300

    feat: day of work
    Criação do

---
+++ airflow/dags/structure_check.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+from datetime import datetime, timedelta
+
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+dag_args = {
+    "owner": "airflow",
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "email": "thi.sil.sab@gmail.com",
+    "retries": 2,
+    "retry_delay": timedelta(minutes=10)
+}
+
+description = "DAG para avaliar o status dos servidores e tabelas antes de executar o processo de etl"
+
+with DAG(
+    dag_id="0001_structure_check",
+    start_date=datetime(2023, 11, 2),
+    schedule_interval="@daily",
+    default_args=dag_args,
+    description=description,
+    catchup=False
+) as dag:
+
+    source_connection = PostgresOperator(
+        task_id="check_src_server",
+        sql="SELECT * FROM t_ponto",
+        postgres_conn_id="source_db_server__postgres"
+    )
+
