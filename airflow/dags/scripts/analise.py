f1a4ef42a0836670467a909273bc402d08e3cfa7
Thiago Sabara <thi.sil.sab@gmail.com>
2023-11-11 17:54:27 -0300

    fix: insert error
    Erro ao inserir dados apos a primeira execução. Corrigido adicionando, if_exists='replace' na chamada do pandas.

--- airflow/dags/scripts/analise.py
+++ airflow/dags/scripts/analise.py
@@ -18,18 +18,18 @@ def run_analysis():
     df = pd.read_sql(src_sql, src_engine)
 
     df_q1 = df.groupby(["username"]).count().id.reset_index()
-    df_q1.to_sql("q1__quantidade_apontamentos_ususario",target_engine, index=False)
+    df_q1.to_sql("q1__quantidade_apontamentos_ususario",target_engine, index=False, if_exists='replace')
 
     df_q2 = df.groupby("company").count().id.reset_index()
-    df_q2.to_sql("q2__quantidade_apontamentos_empresa",target_engine, index=False )
+    df_q2.to_sql("q2__quantidade_apontamentos_empresa",target_engine, index=False, if_exists='replace' )
 
     df_q3 = df_q1.sort_values("id", ascending=False)[:10]
-    df_q3.to_sql("q3__top10_usuarios",target_engine, index=False )
+    df_q3.to_sql("q3__top10_usuarios",target_engine, index=False, if_exists='replace')
 
     df_q4 = df.copy()
     df_q4["insert_date"] = df_q4["insert_date"].apply(lambda x: x.date())
     df_q4 = df_q4.groupby('insert_date').count().id.reset_index()
-    df_q4.to_sql("q4__media_diaria",target_engine, index=False )
+    df_q4.to_sql("q4__media_diaria",target_engine, index=False, if_exists='replace')
 
 if __name__ == '__main__':
     run_analysis()
